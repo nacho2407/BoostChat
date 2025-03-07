6f39956509ee101f2173c92a45012422965093ca
Jang Minjun <jmj0ok@pusan.ac.kr>
2025-03-08 00:02:40 +0900

    Add deleted functions: move constructor and assignment operator of boost_chat::Logger

--- server/src/boost_chat/logger.hpp
+++ server/src/boost_chat/logger.hpp
@@ -26,7 +26,9 @@
 
                         // Deleted on purpose
                         Logger(const Logger&) = delete;
+                        Logger(Logger&&) = delete;
                         Logger& operator =(const Logger&) = delete;
+                        Logger& operator =(Logger&&) = delete;
                 private:
                         Logger() = default;
                 };
