31679e103e7c8090d9fe0ce6a0f20889bc632971
Jang Minjun <jmj0ok@pusan.ac.kr>
2025-03-12 13:10:47 +0900

    Add serializability to message container

--- server/src/boost_chat/message.cpp
+++ server/src/boost_chat/message.cpp
@@ -1,11 +1,7 @@
 #include "message.hpp"
 
-#include <utility>
-
-boost_chat::Message::Message(std::string cid, std::string&& msg, std::string&& time, std::string version): cid_(cid), msg_(std::move(msg)), time_(std::move(time)) {}
-
 template<class Archive>
-void boost_chat::Message::serialize(Archive& ar)
+void boost_chat::Message::serialize(Archive& ar, const unsigned int version)
 {
         ar & cid_;
         ar & msg_;
--- server/src/boost_chat/message.hpp
+++ server/src/boost_chat/message.hpp
@@ -3,14 +3,19 @@
 
 #define BOOST_CHAT_MESSAGE_VERSION "1.0"
 
+#include <boost/serialization/string.hpp>
+
 #include <string>
+#include <utility>
 
 namespace boost_chat
 {
         class Message
         {
         public:
-                Message(std::string cid, std::string&& msg, std::string&& time, std::string version = BOOST_CHAT_MESSAGE_VERSION);
+                Message() = default;
+                Message(std::string cid, std::string&& msg, std::string&& time, std::string version = BOOST_CHAT_MESSAGE_VERSION)
+                        : cid_(cid), msg_(std::move(msg)), time_(std::move(time)) {}
 
                 std::string cid_;
                 std::string msg_;
@@ -18,7 +23,7 @@ namespace boost_chat
                 std::string version_;
 
                 template<class Archive>
-                void serialize(Archive& ar);
+                void serialize(Archive& ar, const unsigned int version);
         };
 }
 #endif
