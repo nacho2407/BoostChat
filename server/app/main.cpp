060bcd3150c046803ec09e3fdf0647ec25a16cbe
Jang Minjun <jmj0ok@pusan.ac.kr>
2025-05-20 22:34:25 +0900

    Update server/app/main and document for BoostChat-server

--- server/app/main.cpp
+++ server/app/main.cpp
@@ -7,45 +7,52 @@
 #include <boost/asio.hpp>
 
 #include <iostream>
+#include <stdexcept>
 #include <stdlib.h>
+#include <string>
 
 /**
  * @brief Start to accept client
  */
-static int run_server(const unsigned short& port);
+static void run_server(const unsigned short port);
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-        unsigned short port = BOOST_CHAT_DEFAULT_PORT;
+    unsigned short port = BOOST_CHAT_DEFAULT_PORT;
 
-        if(argc > 2) {
-                std::cerr << "Usage: BoostChat-server [port=" << BOOST_CHAT_DEFAULT_PORT << "]\n";
+    if (argc > 2) {
+        std::cerr << "Usage: BoostChat-server [port = " << BOOST_CHAT_DEFAULT_PORT << "]\n";
 
-                exit(1);
+        std::exit(1);
+    } else if (argc == 2) {
+        try {
+            port = std::stoi(arg[1]);
+        } catch (const std::invalid_argument &e) {
+            std::cerr << "Invalid port number.\n"
+                      << "Usage: BoostChat-server [port = " << BOOST_CHAT_DEFAULT_PORT << "]\n";
+
+            std::exit(1);
         }
-        else if(argc == 2)
-                port = atoi(argv[1]);
+    }
 
-        std::cout << "================================\n\n"
-        
-        << "\tBoostChat-server\n\n"
+    std::cout << "================================\n\n"
 
-        << "\tver. " << BOOST_CHAT_SERVER_VERSION << "\n\n"
+              << "\tBoostChat-server\n\n"
 
-        << "================================" << std::endl;
-        
-        run_server(port);
+              << "\tver. " << BOOST_CHAT_SERVER_VERSION << "\n\n"
 
-        return 0;
+              << "================================" << std::endl;
+
+    run_server(port);
+
+    return 0;
 }
 
-static int run_server(const unsigned short& port)
+static void run_server(const unsigned short port)
 {
-        boost::asio::io_context context;
+    boost::asio::io_context context;
 
-        boost_chat::Server server(context, port);
+    boost_chat::Server server(context, port);
 
-        context.run();
-        
-        return 0;
+    context.run();
 }
